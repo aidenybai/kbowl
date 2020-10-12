158fb7b923c998bb221458151161e05b1d90501c
abai <mail.contraband@gmail.com>
2020-10-11 22:20:14 -0700

    Clean up room ui

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 function join() {
   const code = prompt('Enter Room Code');
-  if (!code || code.length !== 4) return;
+  if (!code || code.length !== 4) return alert('Invalid code (4 digit uppercase letter). Ask your host for the code.');
   location = `${location.origin}/${code.toUpperCase()}`;
 }
