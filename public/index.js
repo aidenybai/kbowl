3e7e8d21c0f11b8ec33ef7c2c0c39805a04d205a
Aiden Bai <mail.contraband@gmail.com>
2020-11-15 23:08:21 -0800

    Update for lucia@0.2.1

--- public/index.js
+++ public/index.js
@@ -1,18 +1,19 @@
-function join() {
-  const code = prompt('Enter Room Code');
-  if (!code || code.length !== 5) return alert('Invalid code (5 digit uppercase letter). Ask your host for the code.');
-  location = `${location.origin}/${code.toUpperCase()}`;
-}
+Lucia.use('App', {
+  makeid(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const makeid = (length) => {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
 
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-};
-
-document.querySelector('#form').action = `/${makeid(5)}`
+    return text;
+  },
+  join() {
+    const code = prompt('Enter Room Code');
+    if (!code || code.length !== 5) {
+      return alert('Invalid code (5 digit uppercase letter). Ask your host for the code.');
+    }
+    location = `${location.origin}/${code.toUpperCase()}`;
+  },
+});
