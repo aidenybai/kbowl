641714cc3842e0ae6abc23f5945394be7b67e309
Aiden Bai <mail.contraband@gmail.com>
2021-04-22 13:03:09 -0700

    add donation link

--- public/index.js
+++ public/index.js
@@ -1,19 +1,33 @@
-Lucia.use('App', {
-  makeid(length) {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const Collapsable = Lucia.component(
+  'Collapsable',
+  ({ args }) => `
+    <details>
+    <summary>${args[0]}</summary>
+    ${args[1]}
+    </details>
+  `
+);
 
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
+const app = Lucia.use(
+  'App',
+  {
+    makeid(length) {
+      let text = '';
+      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    return text;
+      for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+
+      return text;
+    },
+    join() {
+      const code = prompt('Enter Room Code');
+      if (!code || code.length !== 5) {
+        return alert('Invalid code (5 digit uppercase letter). Ask your host for the code.');
+      }
+      location = `${location.origin}/${code.toUpperCase()}`;
+    },
   },
-  join() {
-    const code = prompt('Enter Room Code');
-    if (!code || code.length !== 5) {
-      return alert('Invalid code (5 digit uppercase letter). Ask your host for the code.');
-    }
-    location = `${location.origin}/${code.toUpperCase()}`;
-  },
-});
+  Collapsable
+);
